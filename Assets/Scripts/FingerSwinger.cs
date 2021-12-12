e6023fcbacf97efeb6d96e3146cd0e8c4e37ca8f
SackingAtlas <harr.blac@gmail.com>
2021-12-11 21:17:29 -0500

    removed trails

--- Assets/Scripts/FingerSwinger.cs
+++ Assets/Scripts/FingerSwinger.cs
@@ -4,16 +4,10 @@ using UnityEngine;
 
 public class FingerSwinger : MonoBehaviour
 {
-    bool isCutting;
-    Rigidbody2D rb;
-    public GameObject TrailPrefab;
-    private GameObject currentTrail;
-
     CircleCollider2D circleCollider;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         circleCollider = GetComponent<CircleCollider2D>();
     }
 
@@ -32,11 +26,11 @@ public class FingerSwinger : MonoBehaviour
             {
                 case TouchPhase.Began:
                     transform.position = new Vector3(worldTouch.x, worldTouch.y, 0);
-                    HitterOn();
                     break;
 
                 case TouchPhase.Moved:
                     transform.position = new Vector3(worldTouch.x, worldTouch.y, 0);
+                    HitterOn();
                     break;
 
                 case TouchPhase.Ended:
@@ -48,14 +42,10 @@ public class FingerSwinger : MonoBehaviour
 
     void HitterOn()
     {
-        isCutting = true;
-        currentTrail = Instantiate(TrailPrefab, transform);
         circleCollider.enabled = true;
     }
     void HitterOff()
     {
-        isCutting = false;
-        Destroy(currentTrail);
         circleCollider.enabled = false;
     }
 
